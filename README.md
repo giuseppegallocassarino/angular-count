d79a8a58b90f9ff4d23804a4da1ea54296ccb6a1
giuseppegallocassarino <giuse.gallo.cass@gmail.com>
2022-03-09 19:58:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ixrnw7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ixrnw7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Counter';
+  count = 42;
+  counter(type: string) {
+    type === 'add' ? this.count++ : this.count--;
+  }
+}
